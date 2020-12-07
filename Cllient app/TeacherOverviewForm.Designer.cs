52e2bab40cd9bd5214d3c95877d761bc23d6b706
Andrey Pantyuhin <52796180+panandafog@users.noreply.github.com>
2020-12-07 11:46:17 +0300

    Teacher's timetable

--- Cllient app/TeacherOverviewForm.Designer.cs
+++ Cllient app/TeacherOverviewForm.Designer.cs
@@ -31,9 +31,14 @@ namespace Cllient_app
         {
             this.userInfoLabel = new System.Windows.Forms.Label();
             this.mainTabControl = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.timetableTabPage = new System.Windows.Forms.TabPage();
+            this.refreshTimetableButton = new System.Windows.Forms.Button();
+            this.examsCheckBox = new System.Windows.Forms.CheckBox();
+            this.timetableGridView = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.mainTabControl.SuspendLayout();
+            this.timetableTabPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timetableGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // userInfoLabel
@@ -47,7 +52,7 @@ namespace Cllient_app
             // 
             // mainTabControl
             // 
-            this.mainTabControl.Controls.Add(this.tabPage1);
+            this.mainTabControl.Controls.Add(this.timetableTabPage);
             this.mainTabControl.Controls.Add(this.tabPage2);
             this.mainTabControl.Location = new System.Drawing.Point(12, 12);
             this.mainTabControl.Name = "mainTabControl";
@@ -55,22 +60,57 @@ namespace Cllient_app
             this.mainTabControl.Size = new System.Drawing.Size(776, 413);
             this.mainTabControl.TabIndex = 1;
             // 
-            // tabPage1
+            // timetableTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 387);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.timetableTabPage.Controls.Add(this.refreshTimetableButton);
+            this.timetableTabPage.Controls.Add(this.examsCheckBox);
+            this.timetableTabPage.Controls.Add(this.timetableGridView);
+            this.timetableTabPage.Location = new System.Drawing.Point(4, 22);
+            this.timetableTabPage.Name = "timetableTabPage";
+            this.timetableTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.timetableTabPage.Size = new System.Drawing.Size(768, 387);
+            this.timetableTabPage.TabIndex = 0;
+            this.timetableTabPage.Text = "Timetable";
+            this.timetableTabPage.UseVisualStyleBackColor = true;
+            // 
+            // refreshTimetableButton
+            // 
+            this.refreshTimetableButton.Location = new System.Drawing.Point(687, 358);
+            this.refreshTimetableButton.Name = "refreshTimetableButton";
+            this.refreshTimetableButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshTimetableButton.TabIndex = 5;
+            this.refreshTimetableButton.Text = "Refresh";
+            this.refreshTimetableButton.UseVisualStyleBackColor = true;
+            this.refreshTimetableButton.Click += new System.EventHandler(this.refreshTimetableButton_Click);
+            // 
+            // examsCheckBox
+            // 
+            this.examsCheckBox.AutoSize = true;
+            this.examsCheckBox.Location = new System.Drawing.Point(6, 362);
+            this.examsCheckBox.Name = "examsCheckBox";
+            this.examsCheckBox.Size = new System.Drawing.Size(57, 17);
+            this.examsCheckBox.TabIndex = 4;
+            this.examsCheckBox.Text = "Exams";
+            this.examsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // timetableGridView
+            // 
+            this.timetableGridView.AllowUserToAddRows = false;
+            this.timetableGridView.AllowUserToDeleteRows = false;
+            this.timetableGridView.AllowUserToOrderColumns = true;
+            this.timetableGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.timetableGridView.Location = new System.Drawing.Point(6, 6);
+            this.timetableGridView.Name = "timetableGridView";
+            this.timetableGridView.ReadOnly = true;
+            this.timetableGridView.Size = new System.Drawing.Size(756, 346);
+            this.timetableGridView.TabIndex = 3;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.Size = new System.Drawing.Size(768, 387);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -86,6 +126,9 @@ namespace Cllient_app
             this.Text = "Teacher Overview";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TeacherOverviewForm_FormClosing);
             this.mainTabControl.ResumeLayout(false);
+            this.timetableTabPage.ResumeLayout(false);
+            this.timetableTabPage.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timetableGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,7 +138,10 @@ namespace Cllient_app
 
         private System.Windows.Forms.Label userInfoLabel;
         private System.Windows.Forms.TabControl mainTabControl;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage timetableTabPage;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button refreshTimetableButton;
+        private System.Windows.Forms.CheckBox examsCheckBox;
+        private System.Windows.Forms.DataGridView timetableGridView;
     }
 }
--- Cllient app/TeacherOverviewForm.cs
+++ Cllient app/TeacherOverviewForm.cs
@@ -16,6 +16,8 @@ namespace Cllient_app
         UserInfo userInfo;
         OleDbConnection connection;
 
+        private System.Data.DataSet dataSet;
+
         public TeacherOverviewForm(UserInfo userInfo, OleDbConnection connection)
         {
             InitializeComponent();
@@ -24,11 +26,53 @@ namespace Cllient_app
             userInfoLabel.Text = this.userInfo.lastName + " " + this.userInfo.firstName;
 
             this.connection = connection;
+
+            dataSet = new DataSet();
+
+            initTimetable();
+            refreshTimetable();
         }
 
         private void TeacherOverviewForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             connection.Close();
+        }
+
+        private void refreshTimetableButton_Click(object sender, EventArgs e)
+        {
+            refreshTimetable();
+        }
+
+        private void initTimetable()
+        {
+            DataTable tmpTable;
+            tmpTable = dataSet.Tables.Add("Timetable");
+            tmpTable.Columns.Add("Date", typeof(DateTime));
+            tmpTable.Columns.Add("Subject", typeof(String));
+            tmpTable.Columns.Add("Teacher's last name", typeof(String));
+            tmpTable.Columns.Add("Teacher's first name", typeof(String));
+            tmpTable.Columns.Add("Classroom", typeof(String));
+            tmpTable.Columns.Add("Building", typeof(String));
+            tmpTable.Columns.Add("Conference", typeof(String));
+            tmpTable.Columns.Add("Conference link", typeof(String));
+
+            timetableGridView.DataSource = dataSet;
+            timetableGridView.DataMember = "Timetable";
+        }
+
+        private void refreshTimetable()
+        {
+            System.Data.OleDb.OleDbDataAdapter adapter;
+
+            dataSet.Tables["Timetable"].Clear();
+
+            String strSQL = "EXEC GetClassesForTeacher @teacherID = " +
+                userInfo.id +
+                ", @isExam = " +
+                examsCheckBox.Checked;
+
+            adapter = new OleDbDataAdapter(strSQL, connection);
+            adapter.Fill(dataSet, "Timetable");
         }
     }
 }
